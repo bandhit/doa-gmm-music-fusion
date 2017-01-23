737e7429c3d8c454d1e8f3db1a2c99bf5b1aa803
Bandhit Suksiri <bandhit.tni@gmail.com>
2017-01-23 13:08:39 +0900

    add library;     - armadillo c++     - LAPACK     - OpenBLAS

--- Workspace/GMM MUFSIC/main.cpp
+++ Workspace/GMM MUFSIC/main.cpp
@@ -1,6 +1,12 @@
+#include <armadillo>
 #include <iostream>
 
 int main (void) {
 	std::cout << "Hello Project" << std::endl;
+	arma::mat a = arma::randu<arma::mat>(5, 5);
+	arma::vec b = arma::randu<arma::vec>(5);
+	arma::vec x;
+	arma::solve(x, a, b);
+	std::cout << x << std::endl;
 	return 0;
 }
